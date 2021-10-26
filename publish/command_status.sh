34b25dc1930d9d1508b1d1047c64aadec8f34882
zengchen1024 <chenzeng2@huawei.com>
2021-10-26 14:47:30 +0800

    update registry of image to push

--- publish/command_status.sh
+++ publish/command_status.sh
@@ -13,7 +13,7 @@ image_tag="v${build_date}-${commit_id}"
 repository=$(pwd | xargs dirname | xargs basename)
 
 cat <<EOF
-IMAGE_REGISTRY ${IMAGE_REGISTRY_OVERRIDE:-swr.ap-southeast-1.myhuaweicloud.com}
+IMAGE_REGISTRY ${IMAGE_REGISTRY_OVERRIDE:-swr.cn-north-4.myhuaweicloud.com}
 IMAGE_REPO ${IMAGE_REPO_OVERRIDE:-opensourceway/robot/$repository}
 IMAGE_TAG ${IMAGE_TAG_OVERRIDE:-$image_tag}
 EOF
